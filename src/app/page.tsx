1673406c83879a854a357b375a0baec250b4391b
azu <azuciao@gmail.com>
2024-11-23 08:23:30 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Page() {
         <div className="container mx-auto px-4">
           <h1 className="text-3xl sm:text-4xl font-bold text-center mb-4 sm:mb-8">JSConf JP 2024 Scheduler</h1>
           <p className="text-center text-muted-foreground mb-4 sm:mb-8">
-            Select sessions from different tracks to create your personal schedule. Sessions run from 10:00 to 18:30.
-            <br/>
-            Official Schedule is available at <a href="https://jsconf.jp/2024/schedule/"
-                                                 className="text-blue-600">https://jsconf.jp/2024/schedule/</a>
+            Select sessions from different tracks to create your personal schedule.
+            <br/>Sessions run from 10:00 to 18:30.
+            <br/>Official Schedule is available at <a href="https://jsconf.jp/2024/schedule/"
+                                                      className="text-blue-600">https://jsconf.jp/2024/schedule/</a>
           </p>
           <ScheduleGrid/>
         </div>
